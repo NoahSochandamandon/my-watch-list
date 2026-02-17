23e8a32a3c5d9821bc45444918db00cc48ff01ad
Noah Sochandamandon <noah.sochandamandon@gmail.com>
2026-02-17 11:49:25 +0100

    feat: intégration API TMDB
    Ajout de l'import automatique de séries par plateforme (Netflix, Prime, Apple). Gestion des doublons via tmdb_id et sécurisation des clés avec python-dotenv.

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 class Task(models.Model):
-	title = models.CharField(max_length=200)
-	complete = models.BooleanField(default=False)
-	created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
 
-	def __str__(self):
-		return self.title
+    tmdb_id = models.IntegerField(unique=True, null=True, blank=True)
+    poster_path = models.CharField(max_length=255, null=True, blank=True)
+    platform = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,43 +1,105 @@
+import requests
 from django.shortcuts import render, redirect
+from django.conf import settings
 from django.http import HttpResponse
 from .models import *
 from .forms import *
+
 # Create your views here.
+
+
+def import_from_tmdb(request, provider_id):
+    added_count = 0
+    page = 1
+
+    providers_names = {8: "Netflix", 119: "Amazon Prime", 350: "Apple TV+"}
+    platform_name = providers_names.get(provider_id, "Inconnu")
+
+    headers = {
+        "Authorization": f"Bearer {settings.TMDB_API_KEY}",
+        "accept": "application/json",
+    }
+
+    while added_count < 10 and page <= 10:
+        url = f"{settings.BASE_URL}/discover/tv"
+        params = {
+            "with_watch_providers": provider_id,
+            "watch_region": "FR",
+            "language": "fr-FR",
+            "sort_by": "popularity.desc",
+            "page": page,
+            "watch_monetization_types": "flatrate",
+        }
+
+        response = requests.get(url, params=params, headers=headers)
+        data = response.json()
+
+        print(
+            f"Status: {response.status_code} | Platform: {platform_name} | Page: {page}"
+        )
+
+        results = data.get("results", [])
+
+        if not results:
+            print("Aucun résultat trouvé ou erreur API")
+            break
+
+        for item in results:
+            if added_count >= 10:
+                break
+
+            # GESTION DES DOUBLONS : On vérifie si le tmdb_id existe déjà
+            if not Task.objects.filter(tmdb_id=item["id"]).exists():
+                Task.objects.create(
+                    title=item["name"],
+                    tmdb_id=item["id"],
+                    poster_path=item.get("poster_path"),
+                    platform=platform_name,
+                    complete=False,
+                )
+                added_count += 1
+
+        page += 1
+
+    return redirect("/")
+
+
 def index(request):
-	tasks = Task.objects.all()
+    tasks = Task.objects.all()
 
-	form = TaskForm()
+    form = TaskForm()
 
-	if request.method == 'POST':
-		form = TaskForm(request.POST)
-		if form.is_valid():
-			#adds to the database if valid
-			form.save()
-		return redirect('/')
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            # adds to the database if valid
+            form.save()
+        return redirect("/")
 
-	context= {'tasks':tasks,'form':form}
-	return render(request, 'tasks/list.html',context)
-
-def updateTask(request,pk):
-	task = Task.objects.get(id=pk)
-	form = TaskForm(instance=task)
-
-	if request.method == "POST":
-		form = TaskForm(request.POST,instance=task)
-		if form.is_valid():
-			form.save()
-			return redirect('/')
+    context = {"tasks": tasks, "form": form}
+    return render(request, "tasks/list.html", context)
 
 
-	context = {'form':form}
-	return render(request, 'tasks/update_task.html',context)
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
 
-def deleteTask(request,pk):
-	item = Task.objects.get(id=pk)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
 
-	if request.method == "POST":
-		item.delete()
-		return redirect('/')
+    context = {"form": form}
+    return render(request, "tasks/update_task.html", context)
 
-	context = {'item':item}
-	return render(request, 'tasks/delete.html', context)
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == "POST":
+        item.delete()
+        return redirect("/")
+
+    context = {"item": item}
+    return render(request, "tasks/delete.html", context)
